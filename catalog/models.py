d51b6703330197e7c6cb96cf52aa303ff1f31e09
Елена Прилуцкая <a.priluckay87@gmail.com>
2024-08-23 23:22:13 +0300

    add models and linter black

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание продукта"
+    )
+    image = models.ImageField(
+        upload_to="product/photo",
+        blank=True,
+        null=True,
+        verbose_name="Фото",
+        help_text="Загрузите фото товара",
+    )
+    category = models.CharField(
+        max_length=100, verbose_name="Категория", help_text="Введите категорию продукта"
+    )
+    price = models.FloatField(
+        verbose_name="Цена продукта", help_text="Введите цену продукта"
+    )
+    created_at = models.DateField(
+        verbose_name="Дата создания (записи в БД)",
+        help_text="Введите дату создания продукта (записи  в БД) ",
+    )
+    updated_at = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name="Дата последнего изменения (записи в БД)",
+        help_text="Введите дату последнего изменения продукта (записи  в БД)",
+    )
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["category", "name", "price", "created_at", "updated_at"]
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Наименование",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание", help_text="Введите описание категории"
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
